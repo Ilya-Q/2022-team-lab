c28659c701667dbd42f55a24ad4b9688a8bf72c6
ThisCakeIsALie <37264026+ThisCakeIsALie@users.noreply.github.com>
2022-06-26 21:31:56 +0200

    Basic implementation of approach (working with around 55% accuracy)

--- approach.py
+++ approach.py
@@ -1,7 +1,8 @@
 import torch
+import torch.nn as nn
 import numpy as np
 from baselines import BaseClassifier
-from sentence_transformers import SentenceTransformer, InputExample, losses, util
+from sentence_transformers import SentenceTransformer, InputExample, losses, models, util
 from torch.utils.data import DataLoader
 from nli_dataset import NLIDataset
 from evaluation import evaluate_model
@@ -48,6 +49,46 @@ class SentenceEmbedClassifier(BaseClassifier):
         else:
             return 2
 
+class KroneckerLoss(nn.Module):
+
+    def __init__(self, model):
+        super().__init__()
+        self.model = model
+        self.loss = nn.MSELoss()
+
+    def occurs_after(self, embed1, embed2):
+        return torch.kron(embed1, embed2)
+
+    def consistent(self, embed):
+        return embed @ torch.tile(torch.tensor([1., -1.]).cuda(), (len(embed) // 2,)) # Can also vary the pattern (i.e. 1 -1 -1 1 or 1 1 -1 -1)
+
+    def forward(self, sentence_features, labels):
+        embeddings = [self.model(sentence_feature)['sentence_embedding'] for sentence_feature in sentence_features]
+        
+        total_loss = torch.tensor(0., requires_grad=True).cuda()
+        for i, label in enumerate(labels):
+            obs1, obs2, hyp1, hyp2 = embeddings[0][i], embeddings[1][i], embeddings[2][i], embeddings[3][i]
+            first_score = self.consistent(self.occurs_after(self.occurs_after(obs1, hyp1), obs2))
+            second_score = self.consistent(self.occurs_after(self.occurs_after(obs1, hyp2), obs2))
+
+            if label == 1:
+                #loss = second_score - first_score
+                loss = self.loss(torch.stack([first_score, second_score]), torch.tensor([1., -1.]).cuda())
+            elif label == 2:
+                loss = self.loss(torch.stack([first_score, second_score]), torch.tensor([-1., 1.]).cuda())
+                #loss = first_score - second_score
+
+            total_loss += loss
+
+            # Add terms that obs1 -> obs2 should be more consistent than obs2 -> obs1 for example (also with hypotheses)
+
+            # Add terms that obs across things should generally have similar consistency
+
+        return total_loss
+
+        
+        
+
 # This should probably move to a separate file...
 class SentenceEmbedder:
     def __init__(self, embedder_path):
@@ -55,11 +96,19 @@ class SentenceEmbedder:
         self.embedder_path = embedder_path
 
     def fit(self, data):
-        new_model = SentenceTransformer('distilbert-base-nli-mean-tokens')
+        word_embedding_model = models.Transformer('bert-base-uncased')
+        pooling_model = models.Pooling(word_embedding_model.get_word_embedding_dimension())
+        dense_model = models.Dense(in_features=pooling_model.get_sentence_embedding_dimension(), out_features=256, activation_function=nn.Tanh())
+
+        new_model = SentenceTransformer(modules=[
+            word_embedding_model,
+            pooling_model,
+            dense_model
+        ])
 
         train_samples = [InputExample(texts=[instance.obs1, instance.obs2, instance.hyp1, instance.hyp2], label=float(instance.label)) for instance in data]
         train_dataloader = DataLoader(train_samples, batch_size=16, shuffle=True)
-        train_loss = losses.CosineSimilarityLoss(new_model) # THIS IS THE MAIN THING WE WOULD LIKE TO CHANGE SO THAT IT IS NOT COSINE SIMILARITY
+        train_loss = KroneckerLoss(new_model) # THIS IS THE MAIN THING WE WOULD LIKE TO CHANGE SO THAT IT IS NOT COSINE SIMILARITY
 
         new_model.fit(train_objectives=[(train_dataloader, train_loss)], epochs=1, warmup_steps=100)
 
@@ -82,6 +131,6 @@ class SentenceEmbedder:
 
 if __name__ == '__main__':
     model = SentenceEmbedClassifier(embedder_path='./data/embeddings')
-    #model.fit(train_data)
+    model.fit(train_data)
 
     print('Approach:', evaluate_model(model, test_data))
