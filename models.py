208474dd4ec1014a078e2d351408d8436abc6453
ThisCakeIsALie <37264026+ThisCakeIsALie@users.noreply.github.com>
2022-07-03 12:12:52 +0200

    Add simple model

--- models.py
+++ models.py
@@ -32,10 +32,24 @@ class KroeneckerSentenceEmbedder(BaseSentenceEmbedder):
         dense_model = models.Dense(in_features=pooling_model.get_sentence_embedding_dimension(), out_features=256, activation_function=nn.Tanh())
         return [backbone, pooling_model, dense_model]
 
-    # TODO: Decide: Should these be based on sentences or on embeddings? (i.e. do we embed them inside this method or does the user have to do it before)
     def occurs_after(self, sentence1, sentence2):
         return torch.kron(sentence1, sentence2)
 
     def consistent(self, sentence):
         return sentence @ torch.tile(torch.tensor([1., -1.]).to(self._target_device), (len(sentence) // 2,)) # Can also vary the pattern (i.e. 1 -1 -1 1 or 1 1 -1 -1)
+
+class SimpleSentenceEmbedder(BaseSentenceEmbedder):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def _build_modules_list(self, backbone):
+        pooling_model = models.Pooling(backbone.get_word_embedding_dimension())
+        dense_model = models.Dense(in_features=pooling_model.get_sentence_embedding_dimension(), out_features=256, activation_function=nn.Tanh())
+        return [backbone, pooling_model, dense_model]
+
+    def occurs_after(self, sentence1, sentence2):
+        return sentence1 + sentence2
+
+    def consistent(self, sentence):
+        return sentence.sum()
 
