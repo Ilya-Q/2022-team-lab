2ad47b7ea261ae75101c8e8a5690f786705a9c36
ThisCakeIsALie <37264026+ThisCakeIsALie@users.noreply.github.com>
2022-07-03 12:12:59 +0200

    Add CE based loss

--- losses.py
+++ losses.py
@@ -30,3 +30,35 @@ class MSELoss(nn.Module):
             # Add terms that obs across things should generally have similar consistency
 
         return total_loss
+
+
+class CELoss(nn.Module):
+    def __init__(self, model):
+        super().__init__()
+        self.model = model
+        self.loss = nn.CrossEntropyLoss()
+
+    def forward(self, sentence_features, labels):
+        embeddings = [self.model(sentence_feature)['sentence_embedding'] for sentence_feature in sentence_features]
+        
+        all_inputs = []
+        all_targets = []
+        for i, label in enumerate(labels):
+            obs1, obs2, hyp1, hyp2 = embeddings[0][i], embeddings[1][i], embeddings[2][i], embeddings[3][i]
+            first_score = self.model.consistent(self.model.occurs_after(self.model.occurs_after(obs1, hyp1), obs2))
+            second_score = self.model.consistent(self.model.occurs_after(self.model.occurs_after(obs1, hyp2), obs2))
+
+            if label == 1:
+                #loss = second_score - first_score
+                input, target = torch.stack([first_score, second_score]), torch.tensor([1., 0.]).to(self.model._target_device)
+            elif label == 2:
+                input, target = torch.stack([first_score, second_score]), torch.tensor([0., 1.]).to(self.model._target_device)
+                #loss = first_score - second_score
+
+                all_inputs.append(input)
+                all_targets.append(target)
+
+        input, target = torch.stack(all_inputs), torch.stack(all_targets)
+        loss = self.loss(input, target)
+
+        return loss
